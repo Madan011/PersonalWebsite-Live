9142db3816658bb4ee3fdce58cfa945095edd81f
madan011 <madan.k.paudel@gmail.com>
2024-02-27 22:36:58 +0545

    Initial Project commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi there !! . This is Madan Kumar Paudel</h1>
+    </div>
+  );
+}
+
+export default App;
