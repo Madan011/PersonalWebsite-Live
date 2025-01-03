63dae108d02a56ee1904ceeff3605aa587ad3de2
madan011 <madan.k.paudel@gmail.com>
2025-01-03 23:04:07 +0545

    update 2 jan 3

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
   return (
     <> 
       <div className='flex min-h-fit bg-gray-300'>
-        <div className='pl-11'><a href=""><img src={logo} alt=""/></a></div>
+        <div className='pl-11'><img src={logo} alt=""/></div>
         <div className='relative ml-auto pr-11'>
 
           <button type='button' className='hover:select-auto' id='menu-button' onClick={ () => showMenu()}><CgMenuRound className='iconmenu'/>
@@ -22,8 +22,8 @@ const NavBar = () => {
 
           <div className={`${isActive ? "NavdropActive" : "NavdropInActive"} `}>
           <div className={'absolute right-6 w-56 rounded-md bg-gray-200 shadow-lg ring-1 ring-black/5 focus:outline-none'} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">         
-          <a href="" className="block px-4 py-2 text-sm maintext hover:bg-gray-300" role="menuitem" tabindex="-1" id="menu-item-0">Project Lookup</a>
-          <a href="" className="block px-4 py-2 text-sm maintext hover:bg-gray-300" role="menuitem" tabindex="-1" id="menu-item-1">About</a>          
+          <div className="block px-4 py-2 text-sm maintext hover:bg-gray-300" role="menuitem" tabindex="-1" id="menu-item-0">Project Lookup</div>
+          <div className="block px-4 py-2 text-sm maintext hover:bg-gray-300" role="menuitem" tabindex="-1" id="menu-item-1">About</div>          
           </div>
           </div>
 
