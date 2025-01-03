e31ce60b0a0e4079e3b0ed9ccb2b4caec164ab28
madan011 <madan.k.paudel@gmail.com>
2025-01-03 22:40:05 +0545

    Update jan 3

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
+import NavBar from './NavBar';
 
 
 const Mainpage = () => {
   return (
     <>
-        <div class="p-2 p-md-3 mb-4 rounded text-body-emphasis bg-body-secondary">
-            <div class="col-lg-6 px-0">
-                <h1 class="display-4 fst-italic">Madan Kumar Paudel</h1>
-                <p class="lead my-3">Dedicated IT Manager with over seven years of experience in the hospitality sector, adept at leveraging technology to enhance guest experiences and optimize operational efficiency.
-                <br/>
-                Part-time Python and React developer passionate about crafting innovative solutions.</p>                
-            </div>
-        </div>
+      <NavBar/> 
+      <h1>Information Technology Manager with more than 7 years of experience in the IT Field delivering
+technical advisory and support to the reputed hotel chain ‘IHG Hotels & Resorts’. Proven success in
+managing high-end network devices, servers, and workstations. Innovative professional with strong
+passion and expertise in Information Technology.<br/>Part-time Python and React developer passionate about crafting innovative solutions.</h1>     
     </>
   )
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from './logo/Logo.svg'
+import {CgMenuRound} from 'react-icons/cg'
+import './CSS/NavBar.css'
 
-const NavBar = () => {
+const NavBar = () => {  
+  
+  const [isActive, setclass] = useState(false)
+  
+  const showMenu = () => {
+      setclass(!isActive)
+  }
+
   return (
-    <>       
-        <h1> Using Rect ROuter DOm Now at NAV BAR</h1>
+    <> 
+      <div className='flex min-h-fit bg-gray-300'>
+        <div className='pl-11'><a href="#"><img src={logo} alt=""/></a></div>
+        <div className='relative ml-auto pr-11'>
+
+          <button type='button' className='hover:select-auto' id='menu-button' onClick={ () => showMenu()}><CgMenuRound className='iconmenu'/>
+          </button>
+
+          <div className={`${isActive ? "NavdropActive" : "NavdropInActive"} `}>
+          <div className={'absolute right-6 w-56 rounded-md bg-gray-200 shadow-lg ring-1 ring-black/5 focus:outline-none'} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">         
+          <a href="#" className="block px-4 py-2 text-sm maintext hover:bg-gray-300" role="menuitem" tabindex="-1" id="menu-item-0">Project Lookup</a>
+          <a href="#" className="block px-4 py-2 text-sm maintext hover:bg-gray-300" role="menuitem" tabindex="-1" id="menu-item-1">About</a>          
+          </div>
+          </div>
+
+          
+
+        </div>
+      </div>      
     </>
   )
 }
